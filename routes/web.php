4ee3e3a5168ac3fe8694d52e3f02bc632fca4721
selfinikmatul <selfinkmtlkh@gmail.com>
2026-05-12 09:43:50 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Barang;
 
 Route::get('/', function () {
-
-    $barang = Barang::all();
-
-    return view('user', compact('barang'));
+    return view('welcome');
 });
