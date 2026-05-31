402bc0f2dd9e8584957c69320d2253f991407538
selfinikmatul <selfinkmtlkh@gmail.com>
2026-05-31 23:18:01 +0700

    ..

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-createApp(App)
-.use(router)
-.mount('#app')
